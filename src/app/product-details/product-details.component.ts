3611d3e4726343090e07edada2ad0016d0fd0435
wsoule <71346930+wsoule@users.noreply.github.com>
2022-09-28 14:30:16 -0400

    add routing to details page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    console.log(routeParams);
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that corresponds with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
